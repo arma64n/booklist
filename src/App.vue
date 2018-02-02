c1ae0fae84c65166d31b343f3b1495f8ae94398d
ginnova <bitbucket@ginnova.kz>
2018-02-02 16:41:45 +0600

    added New Book Form

--- src/App.vue
+++ src/App.vue
@@ -16,40 +16,68 @@
           <td>{{book.isbn}}</td>
           <td>{{book.title}}</td>
           <td>{{book.year}}</td>
-          <td>{{book.authors}}</td>
-          <td><button type="button" class="btn btn-primary">Edit</button></td>
+          <td>{{book.author}}</td>
+          <!--<td><button type="button" class="btn btn-primary" @click="editItem(index)">Edit</button></td>-->
           <td><button type="button" class="btn btn-danger" @click="deleteItem(index)">Delete</button></td>
         </tr>
       </tbody>
     </table>
-    <button type="button" class="btn btn-success">Add new book</button>
-    <new-book-form></new-book-form>
-    <router-view/>
+    <button type="button" class="btn btn-success" @click="visibleNew = true">Add new book</button>
+    <transition name="modal">
+      <new-book-form v-if="visibleNew" v-on:closeModalNew="closeModalNew" v-on:saveNew="saveNew"></new-book-form>
+    </transition>
+    <!--<edit-book-form
+      v-if="visible"
+      :selectedBook="selectedBook"
+      v-on:saveEdit="newBookValue">
+    </edit-book-form> -->
   </div>
 </template>
 
 <script>
 import NewBookForm from '@/components/NewBookForm'
+import EditBookForm from '@/components/EditBookForm'
 
 export default {
   name: 'App',
   data () {
     return {
       books: [
-        {title: 'Lord of The Rings', year: '2002', isbn: '145908562986', authors: 'Tolkien'},
-        {title: 'Turkish gambit', year: '2014', isbn: '520239525231', authors: 'Boris Akunin'}
-      ]
+        {title: 'Lord of The Rings', year: '2002', isbn: '145908562986', author: 'Tolkien'},
+        {title: 'Turkish gambit', year: '2014', isbn: '520239525231', author: 'Boris Akunin'}
+      ],
+      selectedBook: null,
+      visible: false,
+      visibleNew: false
     }
   },
   methods: {
     deleteItem (index) {
       this.books.splice(index, 1)
+    },
+    editItem (index) {
+      this.visible = true
+      this.selectedBook = this.books[index]
+    },
+    newBookValue (value) {
+      this.books.push(value)
+    },
+    saveNew (value) {
+      this.books.push(value)
+    },
+    closeModalNew () {
+      this.visibleNew = false
     }
   },
-  components: { NewBookForm }
+  components: { NewBookForm, EditBookForm }
 }
 </script>
 
 <style>
-
+.modal-enter-active, .modal-leave-active {
+  transition: opacity .5s;
+}
+.modal-enter, .modal-leave-to {
+  opacity: 0;
+}
 </style>
--- src/components/NewBookForm.vue
+++ src/components/NewBookForm.vue
@@ -1,28 +1,59 @@
 <template>
-  <div class="modal fade show" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+  <div class="modal" tabindex="-1" role="dialog" style="display: block">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Add new book</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true" @click="closeModalNew">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" v-model="newBook.isbn" placeholder="ISBN" aria-label="Username" aria-describedby="basic-addon1">
         </div>
-        <div class="modal-body">
-          ...
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" v-model="newBook.title" placeholder="Title" aria-label="Username" aria-describedby="basic-addon1">
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" v-model="newBook.year" placeholder="Year" aria-label="Username" aria-describedby="basic-addon1">
         </div>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" v-model="newBook.author" placeholder="Author" aria-label="Username" aria-describedby="basic-addon1">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" @click="saveNew">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="closeModalNew">Close</button>
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'NewBookForm'
+  name: 'NewBookForm',
+  data () {
+    return {
+      newBook: {}
+    }
+  },
+  methods: {
+    saveNew () {
+      this.$emit('saveNew', this.newBook)
+      this.$emit('closeModalNew')
+      this.newBook = {}
+    },
+    closeModalNew () {
+      this.$emit('closeModalNew')
+    }
+  }
 }
-
 </script>
+
+<style>
+  .modal {
+    background: rgba(0,0,0,0.5)
+  }
+</style>
